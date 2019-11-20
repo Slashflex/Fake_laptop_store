6e8b5dced886d31ade99d9972e92f5d9970ef7b5
slashflex <pro.davidsaoud@gmail.com>
2019-11-20 20:57:37 +0100

    maps thru data.json & displays in template-card

--- index.js
+++ index.js
@@ -11,53 +11,70 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id; // Read ID from url
 
+    //PRODUCTS
     if (pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
 
-        fs.readFile('./public/overview.html', 'UTF-8', (err, html) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(html);
+        fs.readFile(`${__dirname}/public/templates/template-overview.html`, 'UTF-8', (err, data) => {
+            let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/public/templates/template-card.html`, 'UTF-8', (err, data) => {
+                
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                
+                res.end(overviewOutput);
+            });
         });
-        
-    } else if (pathName === '/laptop' && id < pathName.length ) {
+
+    } 
+    
+    // LAPTOP DETAIL
+    else if (pathName === '/laptop' && id < laptopData.length ) {
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
 
         fs.readFile(`${__dirname}/public/templates/template-laptop.html`, 'UTF-8', (err, data) => {
             const laptop = laptopData[id];
-            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-            output = output.replace(/{%IMAGE%}/g, laptop.image);
-            output = output.replace(/{%PRICE%}/g, laptop.price);
-            output = output.replace(/{%SCREEN%}/g, laptop.screen);
-            output = output.replace(/{%CPU%}/g, laptop.cpu);
-            output = output.replace(/{%STORAGE%}/g, laptop.storage);
-            output = output.replace(/{%RAM%}/g, laptop.ram);
-            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            const output = replaceTemplate(data, laptop);
         
             res.end(output);
         });
 
-    } else if (pathName.match('\.css$')) {
+    } 
+    
+    // INCLUDE CSS FILE
+    else if (pathName.match('\.css$')) {
 
         const cssPath = path.join(__dirname, 'public', pathName);
         const fileStream = fs.createReadStream(cssPath, 'UTF-8');
         res.writeHead(200, { 'Content-Type': 'text/css' });
         fileStream.pipe(res);
 
-    } else if (pathName.match('\.jpg$')) {
+    } 
+    
+    // INCLUDE JPG FILES
+    else if (pathName.match('\.jpg$')) {
 
         const imagePath = path.join(__dirname, './public/img', pathName);
         const fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, { 'Content-Type': 'image/jpg' });
         fileStream.pipe(res);
 
-    } else if (pathName.match('\.png$')) {
+    } 
+    
+    // INCLUDE PNG FILES
+    else if (pathName.match('\.png$')) {
 
         const imagePath = path.join(__dirname, './public/img', pathName);
         const fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, { 'Content-Type': 'image/png' });
         fileStream.pipe(res);
 
-    }else {
+    } 
+    
+    // URL NOT FOUND
+    else {
 
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end('No Page Found');
@@ -65,6 +82,20 @@ const server = http.createServer((req, res) => {
     }
 });
 
+// 127.0.0.1:1337
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
 });
+
+const replaceTemplate = (originalHtml, laptop) => {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
