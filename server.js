e89d05c68f3219c7016d3f42cdebad4933409191
slashflex <pro.davidsaoud@gmail.com>
2019-11-21 11:27:11 +0100

    removed local url

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const server = http.createServer((req, res) => {
 });
 
 // 127.0.0.1:1337
-server.listen(process.env.PORT || 1337, '127.0.0.1', () => {
+server.listen(process.env.PORT || 1337, () => {
     console.log('Listening for requests now');
 });
 
